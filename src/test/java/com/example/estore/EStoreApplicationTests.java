b24a27df7549f2cf97768e40e7f0bad94e719131
moeenaslam <moeenkashmiri@gmail.com>
2022-08-04 14:58:17 +0500

    Added simple test case

--- src/test/java/com/example/estore/EStoreApplicationTests.java
+++ src/test/java/com/example/estore/EStoreApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.estore;
 
+import com.example.estore.api.ProductController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EStoreApplicationTests {
 
+  @Autowired
+  private ProductController productController;
+
   @Test
   void contextLoads() {
+  }
+
+
+  @Test
+  void basicTest(){
+    assertThat(productController).isNotNull();
   }
 
 }
